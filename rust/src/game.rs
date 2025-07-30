a9454843636dac82ded78d6ba810b8d8c0cd9a23
newt <hi@newty.dev>
2025-07-30 21:42:51 +0100

    feat(game): scoring

--- rust/src/game.rs
+++ rust/src/game.rs
@@ -1,24 +1,33 @@
 use crate::page::{Page, SHIFT1};
-use godot::prelude::*;
+use godot::{classes::Label, prelude::*};
 
 #[derive(GodotClass)]
 #[class(base = Node)]
 struct Game {
     base: Base<Node>,
-    current_page: Page,
+    /// The current page being displayed.
+    page: Page,
+    /// The pages left in the current shift.
     pages_left: Vec<Page>,
+    /// The player's score.
+    score: isize,
+
+    /// The label to display the score.
+    #[export]
+    score_label: Option<Gd<Label>>,
 }
 
 #[godot_api]
 impl INode for Game {
     fn init(base: Base<Node>) -> Self {
-        let mut game = Self {
+        let mut pages_left = SHIFT1.to_vec();
+        Self {
             base,
-            current_page: Page::new(false), // placeholder
-            pages_left: SHIFT1.to_vec(),
-        };
-        game.next_page();
-        game
+            page: Page::select(&mut pages_left),
+            pages_left,
+            score: 0,
+            score_label: None,
+        }
     }
 }
 
@@ -28,20 +37,29 @@ impl Game {
             // todo: next shift
             self.pages_left = SHIFT1.to_vec();
         }
-        let i = rand::random_range(0..self.pages_left.len());
-        self.current_page = self.pages_left.remove(i);
+        self.page = Page::select(&mut self.pages_left);
+    }
+
+    fn update_score(&mut self, dx: isize) {
+        self.score += dx;
+        if let Some(label) = self.score_label.as_mut() {
+            label.set_text(&format!("Score: {}", self.score));
+        }
     }
 }
 
 #[godot_api]
 impl Game {
     #[func]
-    fn handle_choice(&mut self, accept: bool) {
-        if accept == self.current_page.accept {
+    fn handle_choice(&mut self, answer: bool) {
+        if self.page.check(answer) {
             godot_print!("praise");
-            self.next_page();
+            self.update_score(self.page.bonus as isize);
         } else {
             godot_print!("scold");
+            self.update_score(-(self.page.penalty as isize));
         }
+
+        self.next_page();
     }
 }
--- rust/src/page.rs
+++ rust/src/page.rs
@@ -1,12 +1,29 @@
-pub const SHIFT1: [Page; 2] = [Page::new(true), Page::new(false)];
+pub(crate) const SHIFT1: [Page; 2] = [Page::new(true, 10, 100), Page::new(false, 100, 10)];
 
 #[derive(Clone)]
-pub struct Page {
-    pub accept: bool,
+pub(crate) struct Page {
+    accept: bool,
+    pub(crate) bonus: u8,
+    pub(crate) penalty: u8,
 }
 
 impl Page {
-    pub const fn new(accept: bool) -> Self {
-        Page { accept }
+    /// Create a new [Page].
+    pub(crate) const fn new(accept: bool, bonus: u8, penalty: u8) -> Self {
+        Page {
+            accept,
+            bonus,
+            penalty,
+        }
+    }
+
+    pub(crate) fn check(&self, answer: bool) -> bool {
+        self.accept == answer
+    }
+
+    /// Select a random [Page] from the provided list and remove it.
+    pub(crate) fn select(pages: &mut Vec<Page>) -> Page {
+        let i = rand::random_range(0..pages.len());
+        pages.remove(i)
     }
 }
