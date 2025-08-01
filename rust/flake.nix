2c98f0991d891490db245acffe9b09940fcc76f4
Alexis Williams <107479436+abstralexis@users.noreply.github.com>
2025-08-01 01:14:22 +0100

    (broken) feat(menu): add some fun to the menu and break other things

---
+++ rust/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "An empty flake template that can be adapted to your environment";
+
+  outputs =
+    { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { packages = [ pkgs.emscripten ]; };
+      }
+    );
+
+  inputs = {
+    # we can save 15mb by using the channel tarball
+    # see: https://deer.social/profile/did:plc:mojgntlezho4qt7uvcfkdndg/post/3loogwsoqok2w
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+
+    # flake management
+    utils.url = "github:numtide/flake-utils";
+  };
+}
--- rust/src/menu.rs
+++ rust/src/menu.rs
@@ -1,9 +1,12 @@
-use godot::prelude::*;
+use godot::{classes::{AudioStream, AudioStreamPlayer2D, ResourceLoader}, prelude::*};
 
 #[derive(GodotClass)]
 #[class(init, base = Node)]
 pub(crate) struct Menu {
     base: Base<Node>,
+    #[export]
+    music_player: Option<Gd<AudioStreamPlayer2D>>,
+    music: Gd<AudioStream>
 }
 
 #[godot_api]
@@ -13,5 +16,21 @@ impl Menu {
         if let Some(tree) = self.base().get_tree().as_mut() {
             tree.change_scene_to_file("res://scenes/game.tscn");
         }
+    }
+
+    #[func]
+    fn on_music_stop(&mut self) {
+        self.music_player
+        .as_mut()
+        .map(|player| {
+            let music = ResourceLoader::singleton()
+            .load("res://assets/music/Stage0.ogg")
+            .and_then(|res| res.try_cast::<AudioStream>().ok())
+            .expect("music must be valid");
+
+            player.stop();
+            player.set_stream(&music);
+            player.play();
+        });
     }
 }
