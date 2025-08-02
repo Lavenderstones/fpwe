ae7c89fe624102a8a93d4c92215ed4de10063711
Alexis Williams <107479436+abstralexis@users.noreply.github.com>
2025-08-02 19:06:26 +0100

    feat(game): restructure for company class

--- godot/gdscript/global.gd
+++ godot/gdscript/global.gd
@@ -2,9 +2,5 @@ extends Node
 
 @onready var Employee = preload("res://gdscript/employee.gd")
 
-class Employees:
-	var mark = Employee.new("res://assets/audio/employee_names/Employee1.ogg", Sanity.SanityLevel.NORMAL)
-	var john = Employee.new("res://assets/audio/employee_names/Employee2.ogg", Sanity.SanityLevel.TIRED)
-	var judas = Employee.new("res://assets/audio/employee_names/Employee3.ogg", Sanity.SanityLevel.TIRED)
-	var panopticon = Employee.new("res://assets/audio/employee_names/Employee4.ogg", Sanity.SanityLevel.INSANE)
-	var hell = Employee.new("res://assets/audio/employee_names/Employee5.ogg", Sanity.SanityLevel.INSANE)
+# because sod using actual pointers i want to be silly
+@onready var current_employee = Employee.get_instance(Employee.Employees.MARK)
